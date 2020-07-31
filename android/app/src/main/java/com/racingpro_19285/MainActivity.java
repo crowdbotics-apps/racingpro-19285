7335288cad8577cf7ddc67ea655a0aef6d34e07b
Crowdbotics <team@crowdbotics.com>
2020-07-31 03:08:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/racingpro_19285/MainActivity.java
@@ -0,0 +1,15 @@
+package com.racingpro_19285;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "racingpro_19285";
+    }
+}
